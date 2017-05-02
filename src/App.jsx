75cc34b5bd9125f822bf6da932ab4a49ef7d71db
Jacques Begin <jacques.andre.begin@gmail.com>
2017-05-02 17:42:09 +0000

    Project set up, render basic app content, incorporates React components to compartmentalize HTML blocks of code.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import React, {Component} from 'react';
+import ChatBar from "./ChatBar.jsx"
+import Message from "./Message.jsx"
+import MessageList from "./MessageList.jsx"
 
 class App extends Component {
   render() {
+    console.log("Rendering <App />");
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <nav className="navbar">
+          <a href="/" className="navbar-brand">Chatty</a>
+        </nav>
+        <main className="messages">
+          {console.log("Rendering <MessageList/>")}
+          <MessageList/>
+          {console.log("Rendering <Message/>")}
+          <Message/>
+        </main>
+        {console.log("Rendering <ChatBar/>")}
+        <ChatBar/>
+      </div>
     );
   }
 }
